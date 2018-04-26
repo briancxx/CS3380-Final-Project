43a72623614137d4cc0203d96c3a8fafcb1a619c
Rdubya54 <ryan.wortmann@yahoo.com>
2018-04-26 18:37:59 -0500

    added add student function

--- model.php
+++ model.php
@@ -35,7 +35,7 @@ class Gradebook_model{
         $password=$_POST["password"];
         $status=$_POST["status"];
         
-        $sql="SELECT * FROM " .$status . " WHERE username='" . $login . "' AND password='" . $password . "'";
+        $sql="SELECT * FROM " .$status . " WHERE Username='" . $login . "' AND Password='" . $password . "'";
         echo $sql;
         if ($result = $this->mysqli->query($sql)) {
 				if ($result->num_rows == 1) {
@@ -58,7 +58,7 @@ class Gradebook_model{
         $assignment_id = $this->mysqli->real_escape_string($assignment_id);
         $student_id = $this->mysqli->real_escape_string($student_id);
         
-        $sql = "INSERT INTO grades (Points, assignmentID, StudentID) VALUES ('$points', '$assignment_id', '$student_id')";
+        $sql = "INSERT INTO grades (Points, AssignmentID, StudentID) VALUES ('$points', '$assignment_id', '$student_id')";
         echo $sql;
         
         if (! $result = $this->mysqli->query($sql)) {
@@ -66,12 +66,39 @@ class Gradebook_model{
                 echo "<p>insert failed</p>";
 			}
     }
+    
+    public function add_student($add_student_data){
+        //$firstname=$_POST["firstname"];
+        //$lastname=$_POST["lastname"];
+        //$login=$_POST["login"];
+        //$password=$_POST["password"];
+        
+        $firstname=$add_student_data[0];
+        $lastname=$add_student_data[1];
+        $login=$add_student_data[2];
+        $username=$add_student_data[3];
+        
+        $firstname = $this->mysqli->real_escape_string($firstname);
+        $lastname = $this->mysqli->real_escape_string($lastname);
+        $login = $this->mysqli->real_escape_string($login);
+        $password = $this->mysqli->real_escape_string($password);
+        
+        $sql = "INSERT INTO students (FirstName,LastName,Username,Password) VALUES ('$firstname', '$lastname', '$login','$password')";
+        echo $sql;
+        
+        if (! $result = $this->mysqli->query($sql)) {
+				$this->error = $this->mysqli->error;
+                echo "<p>insert failed</p>";
+			}   
+    }
 }
 
 //$add_grade_data = array("69", "69", "69");
+$add_student_data=array("Steve","Smith","ss","password");
 
 $gradebook_model=new Gradebook_model();
 $gradebook_model->check_login();
 //$gradebook_model->add_grade($add_grade_data);
+$gradebook_model->add_student($add_student_data);
 
 ?>
