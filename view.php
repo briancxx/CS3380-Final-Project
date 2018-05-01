2fc907df53cfdd8cdac7dc325505e0095044afdc
Brian Cox <Brian@mwc-116047.dhcp.missouri.edu>
2018-05-01 17:31:18 -0500

    Added view

---
+++ view.php
@@ -0,0 +1,110 @@
+<?php
+  class Gradebook_views
+  {
+      private $stylesheet = 'styles.css';
+      private $pageTitle = 'Gradebook';
+
+      public function __construct()
+      {
+      }
+
+      public function __destruct()
+      {
+      }
+
+      public function taskListView($students, $grades, $message)
+      {
+        $body = "<h1>Gradebook</h1>"
+
+        // CREATE STUDENTS TABLE ---
+
+        $body .= "<table>";
+
+        foreach ($students as $student) {
+            $id = '';
+            $username = '';
+            $firstName = '';
+            $lastName = '';
+
+            $body .= "<tr>";
+            $body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='add_grade' /><input type='hidden' name='id' value='$id' /><input type='submit' value='Add Grade'></form></td>";
+            $body .= "<td>$username</td><td>$firstName</td><td>$lastName</td>";
+            $body .= "<tr>";
+        }
+
+        $body .= "</table>";
+
+        // CREATE GRADES TABLE ---
+
+        $body .= "<table>"
+
+          foreach ($grades as $grade) {
+              $id = '';
+              $firstName = '';
+              $lastName = '';
+              $assignmentName = '';
+              $pointsEarned = '';
+              $totalPoints = '';
+
+              $body .= "<tr>";
+              $body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='delete_grade' /><input type='hidden' name='id' value='$id' /><input type='submit' value='Delete'></form></td>";
+              $body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='edit_grade' /><input type='hidden' name='id' value='$id' /><input type='submit' value='Edit'></form></td>";
+              $body .= "<td>$firstName</td><td>$lastName</td><td>$assignmentName</td><td>$pointsEarned</td><td>$totalPoints</td>";
+              $body .= "</tr>\n";
+          }
+
+          $body .= "</table>"
+
+          return $this->page($body);
+      }
+
+      public function loginFormView($data = null, $message = '') {
+  			$loginID = '';
+  			if ($data) {
+  				$loginID = $data['loginid'];
+  			}
+
+  			$body = "<h1>Gradebook</h1>\n";
+
+  			if ($message) {
+  				$body .= "<p class='message'>$message</p>\n";
+  			}
+
+  			$body .= <<<EOT
+  <form action='index.php' method='post'>
+  <input type='hidden' name='action' value='login' />
+  <p>User ID<br />
+    <input type="text" name="loginid" value="$loginID" placeholder="login id" maxlength="50" size="50"></p>
+  <p>Title<br />
+    <input type="password" name="password" value="" placeholder="password" maxlength="255" size="80"></p>
+    <input type="submit" name='submit' value="Login">
+  </form>
+  EOT;
+
+  			return $this->page($body);
+  		}
+
+      public function gradeFormView($data, $message) {
+
+      }
+
+      public function studentFormView($data, $message) {
+
+      }
+
+      private function page($body) {
+  			$html = <<<EOT
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <title>{$this->pageTitle}</title>
+  <link rel="stylesheet" type="text/css" href="{$this->stylesheet}">
+  </head>
+  <body>
+  $body
+  </body>
+  </html>
+  EOT;
+  			return $html;
+  		}
+  }
